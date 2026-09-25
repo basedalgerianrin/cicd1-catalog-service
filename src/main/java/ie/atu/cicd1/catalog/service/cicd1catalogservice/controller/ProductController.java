63d81b448de322fad9dc862b6a09d14bd26c52c5
Sami Hammar <G00393324@atu.ie>
2026-09-25 05:30:15 +0100

    Added a lot Product, ProductController, and ProductService

---
+++ src/main/java/ie/atu/cicd1/catalog/service/cicd1catalogservice/controller/ProductController.java
@@ -0,0 +1,4 @@
+package ie.atu.cicd1.catalog.service.cicd1catalogservice.controller;
+
+public class ProductController {
+}
---
+++ src/main/java/ie/atu/cicd1/catalog/service/cicd1catalogservice/model/Product.java
@@ -0,0 +1,16 @@
+package ie.atu.cicd1.catalog.service.cicd1catalogservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
