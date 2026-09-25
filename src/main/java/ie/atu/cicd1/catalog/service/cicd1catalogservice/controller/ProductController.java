0df8cee6a56b819eb4c069e245e2535f3f4740dc
Sami Hammar <G00393324@atu.ie>
2026-09-25 05:59:30 +0100

    Populated ProductController

--- src/main/java/ie/atu/cicd1/catalog/service/cicd1catalogservice/controller/ProductController.java
+++ src/main/java/ie/atu/cicd1/catalog/service/cicd1catalogservice/controller/ProductController.java
@@ -1,4 +1,32 @@
 package ie.atu.cicd1.catalog.service.cicd1catalogservice.controller;
 
+import ie.atu.cicd1.catalog.service.cicd1catalogservice.model.Product;
+import ie.atu.cicd1.catalog.service.cicd1catalogservice.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    public List<Product> getProduct(){
+        return productService.getAll();
+    }
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productService.getAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.create(product);
+    }
 }
